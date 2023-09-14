30ed305181f412f1c26cafccf4d5f21011aa7635
Jarred Muller/Snow <j.muller@live.com.au>
2023-09-14 13:41:57 +1000

    prac 1 sales_bonus.py excercise completed

--- prac_01/sales_bonus.py
+++ prac_01/sales_bonus.py
@@ -4,11 +4,47 @@ If sales are under $1,000, the user gets a 10% bonus.
 If sales are $1,000 or over, the bonus is 15%.
 """
 
-sales = float(input("Enter sales: $"))
+#Create list to store all bonuses and sales for future use or validation
+#set initial sales value to initialise loop
+bonuses= []
+sales = []
 
-if sales < 1000:
-    bonus = sales * 0.1
-    print(f"Bonus: ${bonus:.2f}")
-else:
-    bonus = sales * 0.15
-    print(f"Bonus: ${bonus:.2f}")
+#indexes for lists
+i = 0
+
+#loop to satisfy algorithm:
+#get sales
+# while sales >= 0
+#     calculate bonus
+#     get sales
+# do next thing
+
+if not sales:
+    sale = sales.append(float(input("Enter sale: $")))
+    while sales[i] >= 0:
+        if sales[i] < 1000:
+            bonus = sales[i] * 0.1
+            print(f"Bonus for this sale: ${bonus:.2f}")
+            bonuses.append(bonus)
+            i= i + 1
+            # # validation
+            #print(i)
+            #print(bonuses)
+        else:
+            bonus = sales[i] * 0.15
+            print(f"Bonus for this sale: ${bonus:.2f}")
+            bonuses.append(bonus)
+            i = i + 1
+            # # validation
+            # print(i)
+            # print(bonuses)
+        sales.append(float(input("Enter sales: $")))
+del sales[i]
+total_sales = sum(sales)
+total_bonuses = sum(bonuses)
+print(f"Total Sales:${total_sales:.2f}")
+print(f"Total Bonuses:${total_bonuses:.2f}")
+
+# # validate storage of data
+# print(sales)
+# print(bonuses)
