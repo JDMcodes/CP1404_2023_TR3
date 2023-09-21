61c39dbcbf7766eeb2c822c8dd0b6709dd3014ba
Jarred Muller/Snow <j.muller@live.com.au>
2023-09-21 12:34:52 +1000

    Add password_stars.py with docstrings and improved variable names

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,20 +1,23 @@
 def main():
+    """Stores and input password and prints a censored copy"""
     password = get_password()
-    print_censored_password(password)
+    print_censored_word(password)
     return
 
 
 def get_password():
-    password = input("Please enter password:")
-    if len(password) < 10 or " " in password:
+    """Validates and stores a password containing no spaces and more than 10 characters"""
+    new_password = input("Please enter new password:")
+    if len(new_password) < 10 or " " in new_password:
         print("please enter a valid password")
+        new_password = input("Please enter password:")
 
-    return password
+    return new_password
 
 
-def print_censored_password(password):
-    for i in range(1, len(password)):
-        print("*")
+def print_censored_word(word):
+    """Prints a censored copy of an input word."""
+    print("*" * len(word))
 
 
 main()
