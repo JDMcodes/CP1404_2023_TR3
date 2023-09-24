8431c857a6c783cb736ff6b947588cc24c0effc9
Jarred Muller/Snow <j.muller@live.com.au>
2023-09-24 22:06:24 +1000

    add .py file lists_files.py

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,3 +1,9 @@
+"""
+Password Check with functions
+
+"""
+
+
 def main():
     """Prints an input password as a censored copy"""
     password = get_password()
@@ -7,12 +13,12 @@ def main():
 
 def get_password():
     """Validates and stores a password containing no spaces and more than 10 characters"""
-    new_password = input("Please enter new password:")
-    if len(new_password) < 10 or " " in new_password:
+    password = input("Please enter new password:")
+    if len(password) < 10 or " " in password:
         print("please enter a valid password")
-        new_password = input("Please enter password:")
+        password = input("Please enter password:")
 
-    return new_password
+    return password
 
 
 def print_censored_word(word):
