74fc42eecb22877fd251c814ad0c604685c1e58e
Jarred <jarred.muller@my.jcu.edu.au>
2023-11-24 22:47:11 +1000

    band done

---
+++ prac_09/band.py
@@ -0,0 +1,37 @@
+"""Band class for CP1404
+
+estimate: 30min
+actual: """
+
+
+class Band:
+    """Band class"""
+
+    def __init__(self, name=""):
+        """Construct a Band with a name."""
+        self.name = name
+        self.members = []
+
+
+    def __str__(self):
+        """Return a string representation of a Band."""
+        return f"{self.name} ({self.members})"
+
+    def __repr__(self):
+        """Return a string representation of a Band, showing the variables."""
+        return str(vars(self))
+
+    def add(self, member):
+        """Add a Musician to the band."""
+        self.members.append(member)
+
+    def play(self):
+        """Return a string showing what the members are playing"""
+        for member in self.members:
+            if not member.instruments:
+                print(f"{member.name} needs an instrument!")
+            else:
+                print(f"{member.name} is playing: {member.instruments[0]}")
+
+
+
