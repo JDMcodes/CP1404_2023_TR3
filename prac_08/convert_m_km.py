b67f22940083b52867edab289b4ddadc560d2c9b
Jarred <jarred.muller@my.jcu.edu.au>
2023-11-24 14:58:52 +1000

    add doc string

--- prac_08/convert_m_km.py
+++ prac_08/convert_m_km.py
@@ -14,21 +14,25 @@ MILES_TO_KM = 1.61
 class MilestoKMApp(App):
     """ MilestoKMApp is a Kivy app that takes a miles input and converts to kilometers """
     def build(self):
+        "build kivy app for conversion"
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_m_km.kv')
         return self.root
 
     def handle_convert(self):
+        """converts miles to km"""
         number = self.get_validated_miles()
         result = number * MILES_TO_KM
         self.root.ids.output_label.text = str(result)
 
     def handle_increment(self, change):
+        "function for handling increment"
         value = self.get_validated_miles() + change
         self.root.ids.input_miles.text = str(value)
         self.handle_convert()
 
     def get_validated_miles(self):
+        """validates input and returns 0 if incorrect"""
         try:
             value = float(self.root.ids.input_miles.text)
             return value
